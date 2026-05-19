0f47a2467b27fa4a2c760ee74b29a5bfe65c2dd0
dtiderko <dennis@tiderko.de>
2026-05-19 21:55:04 +0200

    implement automatic camera zoom

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ fn main() {
             toggle_wireframe.run_if(input_just_pressed(KeyCode::Space)),
         )
         .add_systems(Update, advance_sim_time)
+        .add_systems(Update, zoom_camera)
         .run();
 }
 
@@ -35,9 +36,42 @@ fn toggle_wireframe(mut wireframe_config: ResMut<Wireframe2dConfig>) {
 }
 
 fn setup_camera_system(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    commands.spawn(Camera2d::default());
 }
 
 fn advance_sim_time(time: Res<Time>, tweaks: Res<Tweaks>, mut sim_time: ResMut<SimTime>) {
     sim_time.0 += time.delta_secs() * tweaks.time_scale;
+}
+
+fn zoom_camera(
+    entities: Query<&Transform, (With<Mesh2d>, Without<Camera2d>)>,
+    camera: Single<(&mut Transform, &mut Projection), With<Camera2d>>,
+    window: Single<&Window>,
+) {
+    const PADDING: f32 = 1.5;
+
+    let (mut pos, mut proj) = camera.into_inner();
+
+    // figure out the area in which all entities are
+    let min = entities.iter().fold(Vec2::splat(f32::MAX), |acc, e| {
+        acc.min(e.translation.truncate())
+    });
+    let max = entities.iter().fold(Vec2::splat(f32::MIN), |acc, e| {
+        acc.max(e.translation.truncate())
+    });
+
+    // position camera in the center of all entities
+    let center = (min + max) * 0.5;
+    pos.translation.x = center.x;
+    pos.translation.y = center.y;
+
+    let window_size = Vec2::new(window.width(), window.height());
+
+    let size = max - min;
+    let x_ratio = size.x / window_size.x;
+    let y_ratio = size.y / window_size.y;
+
+    if let Projection::Orthographic(orth_proj) = &mut *proj {
+        orth_proj.scale = f32::max(x_ratio, y_ratio) * PADDING;
+    }
 }
