039aa4385be94656a46210400edb4abdd00a3b6c
dtiderko <dennis@tiderko.de>
2026-05-19 17:05:20 +0200

    simplified flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    advisory-db = {
-      url = "github:rustsec/advisory-db";
-      flake = false;
-    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,11 +10,8 @@
 
   outputs =
     {
-      self,
       nixpkgs,
-      crane,
       flake-utils,
-      advisory-db,
       rust-overlay,
       ...
     }:
@@ -28,35 +20,38 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [
+            (import rust-overlay)
+            (_: p: {
+              my-rust = p.rust-bin.stable.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+                targets = [
+                  "x86_64-unknown-linux-gnu"
+                  "wasm32-unknown-unknown"
+                ];
+              };
+
+              rundyn = p.writeShellScriptBin "rundyn" ''
+                cargo run --features bevy/dynamic_linking
+              '';
+            })
+          ];
         };
-        inherit (pkgs) lib;
-
-        craneLib = (crane.mkLib pkgs).overrideToolchain (
-          p:
-          p.rust-bin.stable.latest.default.override {
-            extensions = [
-              "rust-src"
-              "rust-analyzer"
-            ];
-            targets = [
-              "x86_64-unknown-linux-gnu"
-              "wasm32-unknown-unknown"
-            ];
-          }
-        );
-        src = craneLib.cleanCargoSource ./.;
-
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
           # compilers & linkers & dependecy finding programs
           nativeBuildInputs = with pkgs; [
             clang
+            http-server
             mold
+            my-rust
             pkg-config
+            rundyn
+            wasm-bindgen-cli_0_2_121
           ];
 
           # libraries
@@ -66,79 +61,11 @@
             libx11
             libxkbcommon
             udev
+            vulkan-loader
             wayland
           ];
-        };
 
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        crate = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
-        # scripts
-        rundyn = pkgs.writeShellScriptBin "rundyn" ''
-          cargo run --features bevy/dynamic_linking
-        '';
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit crate;
-
-          # check code style
-          crate-clippy = craneLib.cargoClippy (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-            }
-          );
-
-          # check docs
-          crate-doc = craneLib.cargoDoc (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              env.RUSTDOCFLAGS = "--deny warnings";
-            }
-          );
-
-          # check formatting
-          crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          # Audit dependencies
-          crate-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-        };
-
-        packages.default = crate;
-        apps.default = flake-utils.lib.mkApp {
-          drv = crate;
-        };
-
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-
-          # additional packages
-          packages = with pkgs; [
-            rundyn
-            http-server
-            wasm-bindgen-cli_0_2_121
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
-            with pkgs;
-            [
-              vulkan-loader
-              libxkbcommon
-            ]
-          );
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       }
     );
